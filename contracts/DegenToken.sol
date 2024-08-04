65aa42ccfcbc39b69c8bf7e5f87c716332bafe1c
onasachi <samuelonanikechisom@gmail.com>
2024-08-04 08:55:36 +0100

    initial commit

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -16,11 +16,10 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
 
     Item[] public items;
 
+    mapping(address => mapping(uint256 => uint256)) public userItems; // Tracks user's redeemed items
+
     constructor(address initialOwner) ERC20("Degen", "DGN") Ownable(initialOwner) {}
 
-    // items[0] = Item(1, "Sword of Valor", 35);
-    // items[1] = Item(2, "Shield of Protection", 47);
-    // items[2] = Item(3, "Potion of Healing", 18);
     function addItem(uint256 id, string memory name, uint256 quantity) public onlyOwner {
         items.push(Item(id, name, quantity));
     }
@@ -39,10 +38,25 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
         Item storage item = items[itemId];
         require(item.id > 0, "Invalid item ID");
         require(item.quantity >= quantity, "Insufficient quantity to redeem");
+        
+        uint256 requiredTokens = calculateTokenCost(itemId, quantity);
+        require(balanceOf(msg.sender) >= requiredTokens, "Insufficient tokens to redeem");
+
+        // Update item quantity and transfer item to the user
         item.quantity -= quantity;
+        userItems[msg.sender][itemId] += quantity;
+
+        // Burn the required amount of tokens
+        burn(requiredTokens);
 
         emit Redeemed(msg.sender, itemId, quantity);
     }
+
+ function calculateTokenCost(uint256 itemId, uint256 quantity) internal pure returns (uint256) {
+    uint256 costPerItem = itemId * 10**18; // Assuming 1 token per item, scaled to 18 decimal places
+    return quantity * costPerItem;
+}
+
 
     function getBalance() public view returns (uint256) {
         return balanceOf(msg.sender);
